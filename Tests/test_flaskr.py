48c177752eac2b9ef4350829fde5f5fe1675af07
LeoGx <lgarrigoux@gmail.com>
2022-09-06 15:27:47 +0200

    Tests set up for basic routes

--- Tests/test_flaskr.py
+++ Tests/test_flaskr.py
@@ -17,4 +17,17 @@ def test_showSummary_route():
         "/showSummary", data=dict(email="admin@irontemple.com")
     )
     assert response.status_code == 200
-    assert response["template"] == "welcome.html"
+    assert b"Summary | GUDLFT Registration" in response.data
+
+
+def test_book():
+    response = app.test_client().get("/book/Spring%20Festival/Iron%20Temple")
+    assert response.status_code == 200
+    assert b"Spring Festival" in response.data
+
+
+# def test_book():
+#     response = app.test_client().post(
+#         "/book/Spring%20Festival/Iron%20Temple", data=dict(places="10")
+#     )
+#     assert response.status_code == 200
