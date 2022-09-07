d437f87d2a1842e991b723b5cf0c6ac31cf570e0
LeoGx <lgarrigoux@gmail.com>
2022-09-07 15:23:04 +0200

    starting to work on issue

--- Tests/test_flaskr.py
+++ Tests/test_flaskr.py
@@ -3,7 +3,7 @@ import tempfile
 
 import pytest
 
-from server import app
+from server import app, loadCompetitions, purchasePlaces, loadClubs
 
 
 def test_index_route():
@@ -26,8 +26,29 @@ def test_book():
     assert b"Spring Festival" in response.data
 
 
-# def test_book():
-#     response = app.test_client().post(
-#         "/book/Spring%20Festival/Iron%20Temple", data=dict(places="10")
-#     )
-#     assert response.status_code == 200
+def test_loadCompetitions():
+    competitions = loadCompetitions()
+    assert competitions[0]["name"] == "Spring Festival"
+
+
+def test_loadClubs():
+    clubs = loadClubs()
+    assert clubs[0]["name"] == "Simply Lift"
+
+
+def test_purchasePlaces():
+    places = 10
+    competitions = loadCompetitions()
+    print(competitions)
+    original_places = competitions[0]["numberOfPlaces"]
+    clubs = loadClubs()
+    original_points = clubs[0]["points"]
+    response = app.test_client().post(
+        "/purchasePlaces",
+        data=dict(
+            places=places, club=clubs[0]["name"], competition=competitions[0]["name"]
+        ),
+    )
+    assert response.status_code == 200
+    assert int(competitions[0]["numberOfPlaces"]) == int(original_places) - places
+    assert b"Great-booking complete!" in response.data
