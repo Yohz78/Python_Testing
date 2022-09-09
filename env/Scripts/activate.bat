bad0924b4031fb2af150df657131cfb942a49fa2
LeoGx <lgarrigoux@gmail.com>
2022-09-09 13:51:05 +0200

    Corrected env

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Leo\Desktop\OC\Projet_11\Python_Testing\env
+set VIRTUAL_ENV=C:\Users\Leo\Desktop\OC\Python_Testing\env
 
 if not defined PROMPT set PROMPT=$P$G
 
